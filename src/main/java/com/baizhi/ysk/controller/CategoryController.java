ec28a99828bf4391d9633308aec0d00c96dbbdd2
ysk <1197802442@qq.com>
2018-12-17 22:09:10 +0800

    "this is ysk V3.0"

--- src/main/java/com/baizhi/ysk/controller/CategoryController.java
+++ src/main/java/com/baizhi/ysk/controller/CategoryController.java
@@ -38,6 +38,8 @@ public class CategoryController {
 
         System.out.println("333333333333333333333");
         System.out.println("111111111111111111111111");
+        System.out.println("22222222222222222222");
+        System.out.println("33333333333333333333");
     }
 
 
